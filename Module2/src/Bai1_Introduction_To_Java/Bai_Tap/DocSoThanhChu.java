d9b2bffdbabaa26cea36197b628b0f8ca70a0ac2
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-07-13 23:29:59 +0700

    Update bai tap Bai 1 - Module 2

---
+++ Module2/src/Bai1_Introduction_To_Java/Bai_Tap/DocSoThanhChu.java
@@ -0,0 +1,41 @@
+package Bai1_Introduction_To_Java.Bai_Tap;
+
+import java.util.Scanner;
+
+public class DocSoThanhChu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number to string: ");
+        int number = scanner.nextInt();
+        switch (number){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+
+        }
+    }
+}
