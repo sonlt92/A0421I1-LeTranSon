e8c0676fc2a78fe5940ea1ba5ae460b7af42f375
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-07-20 14:17:13 +0700

    Update bai tap Bai 3 - Module 2

---
+++ Module2/src/Bai3_Mang_Va_Phuong_Thuc/Bai_Tap/TongCacSoOMotCot.java
@@ -0,0 +1,50 @@
+package Bai3_Mang_Va_Phuong_Thuc.Bai_Tap;
+
+import java.util.Scanner;
+
+public class TongCacSoOMotCot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Khai bao so dong va cot cua mang 2 chieu
+        int row, col;
+        System.out.print("Input row of Array: ");
+        row = scanner.nextInt();
+        System.out.print("Input column of Array: ");
+        col = scanner.nextInt();
+
+        //Khai bao mang 2 chieu
+        int[][] Arr = new int[row][col];
+
+        System.out.print("Input the element of Array: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("\nArr[" + i + "][" + j + "] = ");
+                Arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        //Hien thi mang 2 chieu
+        System.out.println("Array:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(Arr[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+
+        int elementCol;
+        int sumCol = 0;
+        System.out.print("Enter column to calculate the total: ");
+        elementCol = scanner.nextInt();
+        for (int i = 0; i < row; i++) {
+            for (int j = elementCol; j < col; j++) {
+//                if (j == elementCol){
+                sumCol += Arr[i][j];
+//                }
+            }
+        }
+        System.out.println("Sum column " + elementCol + " is: " + sumCol);
+    }
+
+}
--- Module2/src/Bai3_Mang_Va_Phuong_Thuc/Thuc_Hanh/DemSoLuongSinhVienThiDo.java
+++ Module2/src/Bai3_Mang_Va_Phuong_Thuc/Thuc_Hanh/DemSoLuongSinhVienThiDo.java
@@ -10,15 +10,28 @@ public class DemSoLuongSinhVienThiDo {
         do {
             System.out.print("Enter a size: ");
             size = scanner.nextInt();
-            if (size > 30){
+            if (size > 30) {
                 System.out.println("Size should not exceed 30");
             }
-        }while (size >30);
+        } while (size > 30);
 
         array = new int[size];
         int i = 0;
-        while (i<array.length){
-            System.out.println("Enter for student");
+        while (i < array.length) {
+            System.out.print("Enter for student " + (i + 1));
+            array[i] = scanner.nextInt();
+            i++;
         }
+
+        int count = 0;
+        System.out.print("List of mark: ");
+        for (int j=0;j<array.length;j++){
+            System.out.print(array[j] + "\t");
+            if (array[j] >=5 && array[j] <= 10){
+                count++;
+            }
+        }
+        System.out.println("The number of students passing the exam is " + count);
+
     }
 }
