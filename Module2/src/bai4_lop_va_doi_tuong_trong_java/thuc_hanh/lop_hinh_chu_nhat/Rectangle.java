c8b2989a34033249bcf88f10bbc08ff8287d9f47
A0421I1-LeTranSon <thaitudienha@ymail.com>
2021-07-25 14:22:30 +0700

    Update thuc hanh Bai 4 - Module 2

---
+++ Module2/src/bai4_lop_va_doi_tuong_trong_java/thuc_hanh/lop_hinh_chu_nhat/Rectangle.java
@@ -0,0 +1,13 @@
+package bai4_lop_va_doi_tuong_trong_java.thuc_hanh.lop_hinh_chu_nhat;
+
+public class Rectangle {
+    double width, height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+}
