a4ac6eff7675d2eccb205c4bbc95f09ff48611de
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-01 00:20:43 +0700

    Update bai tap Case Study - Module 2

---
+++ Module2/src/case_study/controllers/Main.java
@@ -0,0 +1,8 @@
+package case_study.controllers;
+
+public class Main {
+    public static void main(String[] args) {
+        FuramaController furamaController = new FuramaController();
+        furamaController.displayMainMenu();
+    }
+}
