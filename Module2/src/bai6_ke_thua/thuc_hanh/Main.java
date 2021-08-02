30769726fbfbbdd299adb13808b6896115135edb
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-02 20:46:22 +0700

    Update thuc hanh Bai 6 - Module 2

---
+++ Module2/src/bai6_ke_thua/thuc_hanh/Main.java
@@ -0,0 +1,38 @@
+package bai6_ke_thua.thuc_hanh;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+
+        shape = new Shape("red", false);
+        System.out.println(shape);
+
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+
+        Square square = new Square();
+        System.out.println(square);
+
+        square = new Square(2.3);
+        System.out.println(square);
+
+        square = new Square(5.8, "yellow", true);
+        System.out.println(square);
+    }
+}
