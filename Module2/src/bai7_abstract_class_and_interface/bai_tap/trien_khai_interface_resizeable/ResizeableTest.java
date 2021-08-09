9ac3291f8654ea78eadec15f2a6843343b35c536
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-09 22:20:00 +0700

    Update bai tap Bai 7 - Module 2

--- Module2/src/bai7_abstract_class_and_interface/bai_tap/trien_khai_interface_resizeable/ResizeableTest.java
+++ Module2/src/bai7_abstract_class_and_interface/bai_tap/trien_khai_interface_resizeable/ResizeableTest.java
@@ -3,7 +3,6 @@ package bai7_abstract_class_and_interface.bai_tap.trien_khai_interface_resizeabl
 public class ResizeableTest {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         Shape[] shapes = new Shape[3];
         shapes[0] = new Circle();
         shapes[1] = new Rectangle();
@@ -11,20 +10,7 @@ public class ResizeableTest {
         for (int i = 0; i < shapes.length; i++) {
             shapes[i].resize(Math.random() * 99 + 1);
             System.out.print("\n" + shapes[i]);
-=======
-        Shape[] shape = new Shape[5];
-        for (int i = 0; i < 5; i++) {
-            shape[i] = getRandomShape();
         }
-        Shape.printShape(shape);
-        for (Shape x: shape){
-            x.resize(Math.random()*100);
->>>>>>> 65f202745445f2277b07669ce54ed9c24886861a
-        }
-
-
     }
-
-
 }
 
