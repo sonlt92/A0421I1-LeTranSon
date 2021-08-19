503a77fa7679e541da30620a073e81b87f1b22db
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-19 22:43:54 +0700

    Update bai tap Module 2 - Bai 10

--- Module2/src/bai10_dsa_danh_sach/bai_tap/trien_khai_arraylist/MyList.java
+++ Module2/src/bai10_dsa_danh_sach/bai_tap/trien_khai_arraylist/MyList.java
@@ -1,6 +1,8 @@
 package bai10_dsa_danh_sach.bai_tap.trien_khai_arraylist;
 
-public class MyList {
+import java.util.Arrays;
+
+public class MyList<E> {
     private int size = 0;
     private static final int DEFAULT_CAPCITY = 10;
     Object elements[];
@@ -17,5 +19,27 @@ public class MyList {
         }
     }
 
+    private int size(){
+        return this.size;
+    }
 
+    private void clear(){
+       size = 0;
+       for (int i=0; i < elements.length; i++){
+           elements[i] = null;
+       }
+    }
+
+    private boolean add(E element){
+        elements[size] = element;
+        size++;
+        return true;
+    }
+
+    private void ensureCapacity(int minCapacity){
+        if (minCapacity >= 0){
+            int newSize = this.elements.length + minCapacity;
+            elements = Arrays.copyOf(elements, newSize);
+        }
+    }
 }
