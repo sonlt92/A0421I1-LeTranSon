c679b20c4bac8e2dda90ecdd9d4b4d476f288da8
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-22 22:30:46 +0700

    Update bai tap Bai 10 - Module 2

--- Module2/src/bai10_dsa_danh_sach/bai_tap/trien_khai_arraylist/MyList.java
+++ Module2/src/bai10_dsa_danh_sach/bai_tap/trien_khai_arraylist/MyList.java
@@ -1,22 +1,21 @@
 package bai10_dsa_danh_sach.bai_tap.trien_khai_arraylist;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class MyList<E> {
-    //So luong phan tu
-    private int size = 0;
     //Suc chua cua mang
     private static final int DEFAULT_CAPACITY = 10;
+    //So luong phan tu
+    private int size = 0;
     //Mang chua phan tu
-    private Object elements[];
+    public Object elements[];
 
     //Suc chua mac dinh khi khoi tao bang contructor
-    public MyList(){
+    public MyList() {
         elements = new Object[DEFAULT_CAPACITY];
     }
 
-    public MyList(int capacity){
+    public MyList(int capacity) {
         if (capacity >= 0) {
             elements = new Object[capacity];
         } else {
@@ -24,6 +23,56 @@ public class MyList<E> {
         }
     }
 
+    public int size() {
+        return this.size;
+    }
+
+    //Phuong thuc clear
+    public void clear() {
+        size = 0;
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+    }
+
+
+    //Phuong thuc add
+    public boolean add(E element) {
+        if (elements.length == size) {
+            this.ensureCapacity(5);
+        }
+        elements[size] = element;
+        size++;
+        return true;
+    }
+
+    public void add(E element, int index) {
+        if (index > elements.length) {
+            throw new IllegalArgumentException("index: " + index);
+        } else if (elements.length == size) {
+            this.ensureCapacity(5);
+        }
+        if (elements[index] == null) {
+            elements[index] = element;
+            size++;
+        } else {
+            for (int i = size + 1; i >= index; i--){
+                elements[i] = elements[i-1];
+            }
+            elements[index] = element;
+            size++;
+        }
+    }
+
+    //Phuong thuc tang kich thuoc
+    public void ensureCapacity(int minCapacity) {
+        if (minCapacity >= 0) {
+            int newSize = this.elements.length + minCapacity;
+            elements = Arrays.copyOf(elements, newSize);
+        } else {
+            throw new IllegalArgumentException("minCapacity: " + minCapacity);
+        }
+    }
 
 
 }
---
+++ Module2/src/bai10_dsa_danh_sach/bai_tap/trien_khai_arraylist/MyListTest.java
@@ -0,0 +1,56 @@
+package bai10_dsa_danh_sach.bai_tap.trien_khai_arraylist;
+
+public class MyListTest {
+
+    public static class Student {
+        private int id;
+        private String name;
+
+        public Student() {
+        }
+
+        public Student(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Student a = new Student(1, "Jacky");
+        Student b = new Student(2, "Ryuk");
+        Student c = new Student(3, "Cuong");
+        Student d = new Student(4, "Quyen");
+        Student e = new Student(5, "Phat");
+        Student f = new Student(6, "Dat");
+
+        MyList<Student> studentMyList = new MyList<>();
+        studentMyList.add(a);
+        studentMyList.add(b);
+        studentMyList.add(c);
+        studentMyList.add(d);
+        studentMyList.add(e);
+        studentMyList.add(f);
+
+        for (int i = 0; i < studentMyList.size(); i++) {
+            Student student = (Student) studentMyList.elements[i];
+            System.out.println(student.getId());
+            System.out.println(((Student) studentMyList.elements[i]).getName());
+        }
+    }
+}
