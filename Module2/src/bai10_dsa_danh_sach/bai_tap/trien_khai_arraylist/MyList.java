07714da854557d4743a96776073007234b8809cf
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-22 17:35:25 +0700

    Update bai tai Bai 10 - Module 2

--- Module2/src/bai10_dsa_danh_sach/bai_tap/trien_khai_arraylist/MyList.java
+++ Module2/src/bai10_dsa_danh_sach/bai_tap/trien_khai_arraylist/MyList.java
@@ -4,8 +4,26 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class MyList<E> {
+    //So luong phan tu
     private int size = 0;
+    //Suc chua cua mang
     private static final int DEFAULT_CAPACITY = 10;
+    //Mang chua phan tu
     private Object elements[];
+
+    //Suc chua mac dinh khi khoi tao bang contructor
+    public MyList(){
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity){
+        if (capacity >= 0) {
+            elements = new Object[capacity];
+        } else {
+            throw new IllegalArgumentException("Capacity: " + capacity);
+        }
+    }
+
+
 
 }
