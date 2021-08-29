37e95a61c5b91fb56c41b559c9cf4c6d94f22a95
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-29 23:17:47 +0700

    Update thuc hanh Bai 12 - Module 2

---
+++ Module2/src/bai12_java_collection_framework/thuc_hanh/comparable_va_comparator/AgeComparator.java
@@ -0,0 +1,16 @@
+package bai12_java_collection_framework.thuc_hanh.comparable_va_comparator;
+
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        if(o1.getAge() > o2.getAge()){
+            return 1;
+        }else if(o1.getAge() == o2.getAge()){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+}
