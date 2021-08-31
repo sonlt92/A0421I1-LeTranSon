afa48d67d8de6b80562d55a2b261310b82f8c1c3
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-08-31 21:33:25 +0700

    Update bai tap Bai 12 - Module 2

--- Module2/src/bai12_java_collection_framework/bai_tap/luyen_tap_su_dung_arraylist_linkedlist/Main.java
+++ Module2/src/bai12_java_collection_framework/bai_tap/luyen_tap_su_dung_arraylist_linkedlist/Main.java
@@ -14,10 +14,18 @@ public class Main {
         product1.setName("Ubuntu");
         product1.setPrice(100);
 
+        Product product2 = new Product();
+        product2.setId(3);
+        product2.setName("Deepin");
+        product2.setPrice(10);
+
         ProductManager productManager = new ProductManager();
         productManager.addProduct(product);
         productManager.addProduct(product1);
+        productManager.addProduct(product2);
         productManager.displayProduct(0);
+        productManager.displayProduct(1);
+        productManager.displayProduct(2);
         CompareTest compareTest = new CompareTest();
 //        Collections.sort(productManager,compareTest);
     }
