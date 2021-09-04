04b15e2844bb124268a502a5759b51de52e641a0
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-09-04 14:38:54 +0700

    Update bai tap Bai 13 - Module 2

--- Module2/src/bai13_thuat_toan_tim_kiem/bai_tap/tim_chuoi_tang_dan/Main.java
+++ Module2/src/bai13_thuat_toan_tim_kiem/bai_tap/tim_chuoi_tang_dan/Main.java
@@ -1,5 +1,6 @@
 package bai13_thuat_toan_tim_kiem.bai_tap.tim_chuoi_tang_dan;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +10,26 @@ public class Main {
         System.out.print("Nhap chuoi: ");
         String string = input.nextLine();
 
+        LinkedList<Character> max = new LinkedList<>();
 
+        for (int i = 0; i < string.length(); i++) {
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(string.charAt(i));
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.charAt(j) > list.getLast()) {
+                    list.add(string.charAt(j));
+                }
+            }
+            if (list.size() > max.size()) {
+                max.clear();
+                max.addAll(list);
+            }
+            list.clear();
+        }
+
+        for (Character ch : max) {
+            System.out.print(ch);
+        }
+        System.out.println();
     }
 }
