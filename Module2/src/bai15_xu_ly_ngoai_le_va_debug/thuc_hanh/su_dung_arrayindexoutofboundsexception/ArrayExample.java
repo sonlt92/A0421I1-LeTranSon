58bb2d002025126865ee6ded93305cf3281d3057
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-09-08 15:06:35 +0700

    Update thuc hanh Bai 15 - Module 2

---
+++ Module2/src/bai15_xu_ly_ngoai_le_va_debug/thuc_hanh/su_dung_arrayindexoutofboundsexception/ArrayExample.java
@@ -0,0 +1,18 @@
+package bai15_xu_ly_ngoai_le_va_debug.thuc_hanh.su_dung_arrayindexoutofboundsexception;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+    public Integer[] createRandom() {
+        Random rd = new Random();
+        Integer[] arr = new Integer[100];
+        System.out.println("Danh sách phần tử của mảng: " );
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rd.nextInt(100);
+            System.out.print(arr[i]+ " ");
+        }
+        return arr;
+    }
+
+}
---
+++ Module2/src/bai15_xu_ly_ngoai_le_va_debug/thuc_hanh/su_dung_arrayindexoutofboundsexception/Main.java
@@ -0,0 +1,19 @@
+package bai15_xu_ly_ngoai_le_va_debug.thuc_hanh.su_dung_arrayindexoutofboundsexception;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayExample arrExample = new ArrayExample();
+        Integer[] arr = arrExample.createRandom();
+
+        Scanner scaner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scaner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số 5" + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+}
