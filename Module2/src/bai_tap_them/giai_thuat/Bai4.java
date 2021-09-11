84d382ebf61aa8a6d69b55e54c74ad97e1a892ea
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-09-11 18:49:02 +0700

    Update bai tap Bai 16 - Module 2

---
+++ Module2/src/bai_tap_them/giai_thuat/Bai4.java
@@ -0,0 +1,19 @@
+package bai_tap_them.giai_thuat;
+
+import java.util.Scanner;
+
+public class Bai4 {
+    //Viet chu KDW bang dau sao
+    public static void main(String[] args) {
+        System.out.println("**        ***    **********      **             *             **");
+        System.out.println("**      ***      **        **     **           ***           ** ");
+        System.out.println("**    ***        **         **     **         ** **         **  ");
+        System.out.println("**  ***          **          **     **       **   **       **   ");
+        System.out.println("*****            **          **      **     **     **     **    ");
+        System.out.println("**  ***          **          **       **   **       **   **     ");
+        System.out.println("**    ***        **         **         ** **         ** **      ");
+        System.out.println("**      ***      **        **           ***           ***       ");
+        System.out.println("**        ***    **********              *             *        ");
+
+    }
+}
