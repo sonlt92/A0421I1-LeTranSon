a49aace71d6432a85a376db2df0d43579c7e7272
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-11-28 23:11:27 +0700

    Update bai tap Bai 1 - Module 4

---
+++ Module4/bai1_spring_mvc/bai_tap/chuyen_doi_tien/src/main/java/letranson/controller/ConvertController.java
@@ -0,0 +1,33 @@
+package letranson.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.text.NumberFormat;
+
+@Controller
+public class ConvertController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homePage() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/convert", method = RequestMethod.GET)
+    public ModelAndView convertMoney(@RequestParam double money) {
+        ModelAndView  convertPage = new ModelAndView("index");
+        double  result = money * 23000;
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        String  resultString = numberFormat.format(result);
+        convertPage.addObject("result", resultString);
+        return convertPage;
+    }
+}
+
+
+
+
