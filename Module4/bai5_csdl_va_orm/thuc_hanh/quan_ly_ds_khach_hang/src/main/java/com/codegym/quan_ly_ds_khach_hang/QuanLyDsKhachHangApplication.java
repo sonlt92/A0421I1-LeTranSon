bcffe5a1f5e67d7dbed06535ba8977ea79694584
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-12-15 23:03:15 +0700

    Update bai tap Bai 4 - Module 4

---
+++ Module4/bai5_csdl_va_orm/thuc_hanh/quan_ly_ds_khach_hang/src/main/java/com/codegym/quan_ly_ds_khach_hang/QuanLyDsKhachHangApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.quan_ly_ds_khach_hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyDsKhachHangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyDsKhachHangApplication.class, args);
+    }
+
+}
---
+++ Module4/bai5_csdl_va_orm/thuc_hanh/quan_ly_ds_khach_hang/src/test/java/com/codegym/quan_ly_ds_khach_hang/QuanLyDsKhachHangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.quan_ly_ds_khach_hang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyDsKhachHangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
