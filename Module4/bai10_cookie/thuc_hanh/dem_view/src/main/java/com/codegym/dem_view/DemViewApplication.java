a55de103dbb899afe90eb5c786306f91289695c9
A0421I1-LeTranSon <letranson1992@gmail.com>
2021-12-31 20:56:05 +0700

    Update thuc hanh Bai 10 - Module 4

---
+++ Module4/bai10_cookie/thuc_hanh/dem_view/src/main/java/com/codegym/dem_view/DemViewApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.dem_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemViewApplication.class, args);
+    }
+
+}
---
+++ Module4/bai10_cookie/thuc_hanh/dem_view/src/test/java/com/codegym/dem_view/DemViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.dem_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
