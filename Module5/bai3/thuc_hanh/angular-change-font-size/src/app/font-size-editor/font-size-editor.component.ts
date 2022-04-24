7547585a6c358ece4d71d28a5c2af4619ba0787a
A0421I1-LeTranSon <letranson1992@gmail.com>
2022-04-24 12:16:57 +0700

    Update bai tap

---
+++ Module5/bai3/thuc_hanh/angular-change-font-size/src/app/font-size-editor/font-size-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-font-size-editor',
+  templateUrl: './font-size-editor.component.html',
+  styleUrls: ['./font-size-editor.component.css']
+})
+export class FontSizeEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
