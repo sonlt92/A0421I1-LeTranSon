9ac32c8f73979ca15eebf251744fd967fa90fc0e
A0421I1-LeTranSon <letranson1992@gmail.com>
2022-04-25 18:32:11 +0700

    Update Demo

---
+++ Module5/bai3/demo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentDetailComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
